371a6a8a6006b14dd7818e842aadaa86fe05fe11
cstp <759168442@qq.com>
2022-04-09 10:16:57 +0800

    feta:配置跨域

--- oa-manager-system/vue.config.js
+++ oa-manager-system/vue.config.js
@@ -3,5 +3,22 @@
  * @date 2022/4/9 10:06 AM
  */
 module.exports = {
-  lintOnSave : false
+  publicPath : "./",
+  devServer : {
+    port : 9998,
+    host : "localhost",
+    https : false,
+    open : true,
+    proxy : {
+      [process.env.VUE_APP_BASE_API] : {
+        target : process.env.VUE_APP_SERVICE_URL,
+        changeOrigin : true,
+        pathRewrite : {
+          ["^" + process.env.VUE_APP_BASE_API] : ""
+        }
+      }
+    }
+  },
+  lintOnSave : false,
+  productionSourceMap : false
 }
