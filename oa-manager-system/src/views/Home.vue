ed2edbbd25bfb1eb4e777ce711a3ed30b0a48eda
cstp <759168442@qq.com>
2022-04-09 10:09:25 +0800

    feta:安装axios以及element-plus

--- oa-manager-system/src/views/Home.vue
+++ oa-manager-system/src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
   </div>
 </template>
 
---
+++ oa-manager-system/vue.config.js
@@ -0,0 +1,7 @@
+/**
+ * @author YangLing
+ * @date 2022/4/9 10:06 AM
+ */
+module.exports = {
+  lintOnSave : false
+}
